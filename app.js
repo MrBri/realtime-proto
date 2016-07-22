94ae7671bcd2f6b34de6deaf2e881e59d4d7e740
Tyler Briles <tyler@mrbri.com>
2016-07-22 11:43:36 -0700

    Hardcode list, section, form

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+var channels = [
+    { name: 'Hardware support' },
+    { name: 'Software support'}
+];
+
 class Channel extends React.Component {
     onClick() {
         console.log('On click working: ', this.props.name)
@@ -9,4 +14,37 @@ class Channel extends React.Component {
     }
 }
 
-ReactDOM.render(<Channel name="Hardware support"/>, document.getElementById('app'));
+class ChannelList extends React.Component {
+    render() {
+        return (
+            <ul>
+                {this.props.channels.map(channel => {
+                    return <Channel name={channel.name} />
+                })}
+            </ul>
+        )
+    }
+}
+
+class ChannelForm extends React.Component {
+    render() {
+        return (
+            <form>
+                <input type="text" />
+            </form>
+        )
+    }
+}
+
+class ChannelSection extends React.Component {
+    render() {
+        return (
+            <div>
+                <ChannelList channels={channels} />
+                <ChannelForm />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<ChannelSection />, document.getElementById('app'));
